6ea558ec04f2b9d447945666cc6018d00000428b
Thibaut <thibaut.andre277@gmail.com>
2021-11-03 20:57:56 +0100

    Use resources/app.scss << public/light.css

--- resources/views/public/template/header.blade.php
+++ resources/views/public/template/header.blade.php
@@ -15,7 +15,7 @@
 	<title>PathologIA</title>
 
 	<link href="{{asset('css/app.css')}}" rel="stylesheet">
-	<link href="{{asset('css/light.css')}}" rel="stylesheet">
+	<!--<link href="{{asset('css/light.css')}}" rel="stylesheet">-->
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
