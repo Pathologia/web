d90ffacdf42b23f0ec394149b61842dae6767e2c
Thibaut <thibaut.andre277@gmail.com>
2021-11-04 22:17:34 +0100

    Remove theme AdminKit

--- resources/views/auth/template/navbar-left.blade.php
+++ resources/views/auth/template/navbar-left.blade.php
@@ -1,33 +1,16 @@
-<nav id="sidebar" class="sidebar js-sidebar text-white bg-medium radius-right" style="overflow-y: hidden; overflow-x: hidden;">
-    <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand text-center" href="{{route('home.show')}}"><span>Patholog <strong>IA</strong></span></a>
-        <div class="sidebar-user">
-            <div class="d-flex justify-content-center">
-                <div class="flex-grow-1 ps-2">
-                    <h3 class="sidebar-user-title">{{$user->firstname}} {{$user->lastname}}</h3>
-                    @foreach ($roles as $role)
-                        @if($role->id === $user->role_id)
-                            <h4 class="sidebar-user-title">{{$role->libelle}}</h4>
-                        @endif
-                    @endforeach
-                </div>
-            </div>
-        </div>
-
-        <ul class="sidebar-nav">
-            <li class="sidebar-header">Menu</li>
-            <li class="sidebar-item @if(Route::is('home.show')) active @endif">
-                <a class="sidebar-link bg-transparent" href="{{route('home.show')}}"><i class="fas fa-home"></i> <span class="align-middle"> Accueil</span></a>
-            </li>
-            <li class="sidebar-header">Compte</li>
-            <li class="sidebar-item @if(Route::is('user.show')) active @endif">
-                <a class="sidebar-link bg-transparent" href="{{route('user.show')}}"><i class="fas fa-users-cog"></i> <span class="align-middle"> Mon profil</span></a>
-            </li>
-            <li class="sidebar-item"><br></li>
-            <li class="sidebar-header">Déconnexion</li>
-            <li class="sidebar-item">
-                <a class="dropdown-item btn text-danger" href="{{route('user.logout')}}"><i class="fas fa-sign-out-alt"></i> Me déconnecter</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+<div class="d-flex flex-column align-items-sm-start px-3 pt-2 min-vh-100 bg-gray">
+    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+        <li><h3>{{$user->firstname}} {{$user->lastname}}</h3></li>
+        <li class="sidebar-header">Menu</li>
+        <li class="nav-item @if(Route::is('home.show')) active @endif">
+            <a class="nav-link bg-transparent" href="{{route('home.show')}}"><i class="fas fa-home"></i> <span class="align-middle">Accueil</span></a>
+        </li>
+        <li class="nav-header">Compte</li>
+        <li class="nav-item @if(Route::is('user.show')) active @endif">
+            <a class="nav-link bg-transparent" href="{{route('user.show')}}"><i class="fas fa-users-cog"></i> <span class="align-middle">Mon profil</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="dropdown-item btn text-danger" href="{{route('user.logout')}}"><i class="fas fa-sign-out-alt"></i>Me déconnecter</a>
+        </li>
+    </ul>
+</div> 
