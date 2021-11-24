009e8819f67f3d2daaea391c209799c7e561e9a2
Maxime LARROZE <maxime@groupeehe.fr>
2021-11-24 11:27:53 +0100

    Correction bug SASS

--- resources/views/template/navbar-top.blade.php
+++ resources/views/template/navbar-top.blade.php
@@ -1,16 +1,21 @@
-<nav class="navbar navbar-expand-sm navbar-dark bg-transparent mb-2">
-    <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="mynavbar">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                    <a href="#menu-toggle" class="mt-5" id="menu-toggle"><i class="fas fa-bars fa-2x"></i></a>
-                </li>
-            </ul>
-            <a class="d-flex nav-icon js-fullscreen" href="#">
-                <div class="position-relative">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize align-middle"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg>
-                </div>
-            </a>
+<nav class="sb-topnav navbar navbar-expand navbar-dark bg-transparent">
+    <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="fas fa-bars fa-2x"></i></button>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><svg class="svg-inline--fa fa-bars fa-w-14" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg><!-- <i class="fas fa-bars"></i> Font Awesome fontawesome.com --></button>
+    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+        <div class="input-group">
+            <input class="form-control" type="text" placeholder="Rechercher" aria-label="Rechercher" aria-describedby="btnNavbarSearch">
+            <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search-plus"></i></button>
         </div>
-    </div>
+    </form>
+    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user"></i></a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li class="text-center">{{$user->firstname}} {{$user->lastname}}</li>
+                <li><hr class="dropdown-divider radius"></li>
+                <li><a class="dropdown-item radius" href=""><i class="fas fa-cogs"></i> Paramètres</a></li>
+                <li><a class="dropdown-item radius text-danger" href="#!"><i class="fas fa-sign-out-alt"></i> Déconnexion</a></li>
+            </ul>
+        </li>
+    </ul>
 </nav>
